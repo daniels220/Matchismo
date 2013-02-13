3220e0ea1c0fb537cbb70ffab4d87bfe428960bd
Daniel Slomovits <daniels220@gmail.com>
2013-02-13 18:57:23 -0500

    Some tweaks to Assignment 1

--- Matchismo/MGViewController.m
+++ Matchismo/MGViewController.m
@@ -36,21 +36,6 @@
 
 @implementation MGViewController
 
-//OVERRIDES
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-//INITIALIZERS
-
 //GET/SET
 
 -(void)setCardButtons:(NSArray *)cardButtons {
